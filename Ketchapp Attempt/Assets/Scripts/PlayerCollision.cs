d53928c02ed5865573ec4ba90a1c85481df20879
PotentiallyParadoxical <fluffyandy97@gmail.com>
2019-05-08 14:22:29 -0700

    Added the project

---
+++ Ketchapp Attempt/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+    public GameObject dead;
+    public float shatterStrength;
+
+
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+
+            //Shatter
+            Rigidbody rig = gameObject.GetComponent<Rigidbody>();
+            Vector3 cubeVelocity = rig.velocity;
+            GameObject deadCube = Instantiate(dead, transform.position, transform.rotation);
+            for (int child = 0; child < deadCube.transform.childCount; child++)
+            {
+                deadCube.transform.GetChild(child).GetComponent<Rigidbody>().velocity = new Vector3(cubeVelocity.x + Random.Range(-shatterStrength,shatterStrength), cubeVelocity.y + Random.Range(-shatterStrength, shatterStrength), cubeVelocity.z + Random.Range(-shatterStrength, shatterStrength));
+            }
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Ketchapp Attempt/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Reference to the Rigidbody component called "rb"
+    void Start()
+    {
+
+    }
+
+    // "Fixed Update" because we are messing wi th physics.
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); 
+
+        if ( Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+    }
+}
