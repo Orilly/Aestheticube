a56317243292ad7ae8caa9d00c178c23bc2b84b1
Keval <therealchubbypanda@gmail.com>
2019-05-26 20:28:58 +0100

    Fixed Music

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    private static MusicController _Instance = null;
+    public static MusicController Instance {
+        get { return _Instance; }
+    }
+
+    void Awake()
+    {
+        if (_Instance != null && _Instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        } else
+        {
+            _Instance = this;
+        }
+
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
