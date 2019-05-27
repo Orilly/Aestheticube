f1ef2c7ec5d68fbc054a40d9fec299c9336c5cb7
PotentiallyParadoxical <fluffyandy97@gmail.com>
2019-05-27 15:41:45 -0700

    Fixed menu stuff

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.Quit();
+        print("The game has been 'quit'. But you are using the editor so the text is here instead.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
