2237151d235bd179df698d4145721f81c767e98b
PotentiallyParadoxical <fluffyandy97@gmail.com>
2019-05-31 12:56:58 -0700

    Coins now save
    I have 5

---
+++ Assets/Scripts/FileEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+public class FileEditor
+{
+    public void WriteString(string path, string text)
+    {
+        File.WriteAllText(path, text);
+    }
+
+    public string ReadString(string path)
+    {
+        string text;
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path);
+        text = reader.ReadToEnd();
+        reader.Close();
+        return text;
+    }
+
+}
