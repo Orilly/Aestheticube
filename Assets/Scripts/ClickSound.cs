6ba442a4931620ec083ac946d7b1aa462438d7dd
PotentiallyParadoxical <fluffyandy97@gmail.com>
2019-06-02 11:17:53 -0700

    ASMR CLICKING
    Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes Yes

---
+++ Assets/Scripts/ClickSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSound : MonoBehaviour
+{
+    public bool click { get; set; }
+    public AudioClip[] clickSounds;
+    public AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (click)
+        {
+            sound.PlayOneShot(clickSounds[Random.Range(0, clickSounds.Length)]);
+            click = false;
+        }
+    }
+}
